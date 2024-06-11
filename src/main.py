8e5a7abc033d5869873541bbc27b78e7e3a46ae9
e8olution <evzen.fc@gmail.com>
2024-06-11 15:25:50 +0200

    .gitignore done pep 8 correct line tests working correct

--- src/main.py
+++ src/main.py
@@ -1,43 +1,52 @@
 
 #создание_класса
 class Category:
- # атрибуты
- #типы_данных
- name: str
- description:str
- products: list
- count_categories = 0
- count_uniq_cat = 0
+    # атрибуты
+    # типы_данных
+    name: str
+    description: str
+    products: list
+    count_categories = 0
+    count_uniq_cat = 0
 
-# конструктор_класса
-# __init__
- def __init__(self,name,description,products):
-#self
-  self.name = name
-  self.description = description
-  self.products = products
-
-  Category.count_uniq_cat +=len (set([product.name for product in products]))
-  Category.count_categories += 1
+    # конструктор_класса
+    # __init__
+    def __init__(self,name,description,products):
+        # self
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.count_uniq_cat += len(set([product.name for product in products]))
+        Category.count_categories += 1
 
 
 #создание_класса
-class Product:
  # атрибуты
  #типы_данных
- name: str
- description: str
- price: float
- quantity: int
+class Product:
+    # атрибуты
+    # типы_данных
+    name: str
+    description: str
+    price: float
+    quantity: int
 
 # конструктор_класса
 # __init__
- def __init__(self,name,description,price,quantity):
-# self
-  self.name = name
-  self.description = description
-  self.price = price
-  self.quantity = quantity
+    def __init__(self,name,description,price,quantity):
+        # self
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+#    def __eq__(self, other):
+#        if isinstance(other, Product):
+#            return (self.name == other.name and
+#                    self.description == other.description and
+#                    self.price == other.price and
+#                    self.quantity == other.quantity)
+#        return False
 
 
 def __repr__(self):
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,29 +1,34 @@
 import pytest
-from src.main import Product,Category
+from src.main import Product
+from src.main import Category
+
 
 
 
 @pytest.fixture
 def category_data():
     return Category('Electronics', 'Smartphones',
-                    [Product('Iphone','New_1', 99.999, 100),
-                     Product('Samsung','New_2', 89.999, 99)])
-def test_init_Category(category_data):
+                    [
+                        Product('Iphone','New_1', 99.999, 100),
+                        Product('Samsung','New_2', 89.999, 99)
+                    ]
+                    )
+def test_init_category(category_data):
     # Проверка корректности инициализации объектов класса Category
     assert category_data.name == 'Electronics'
     assert category_data.description == 'Smartphones'
-    assert category_data.products == [Product('Iphone','New_1', 99.999, 100),
-                     Product('Samsung','New_2', 89.999, 99)]
+    assert category_data.description == 'Smartphones'
+    assert len(category_data.products) == 2
 
-    assert category_data.count_uniq_cat == 1
-    assert category_data.count_categories == 2
+    assert category_data.count_uniq_cat == 2
+    assert category_data.count_categories == 1
 
 @pytest.fixture
 def product_data():
-    return Product('Electronics','Smartphones',99.999, 100)
-def test_init_Product(product_data):
+    return Product('Samsung','A mobile phone with advanced features',999.999, 100)
+def test_init_product(product_data):
     assert product_data.name == "Samsung"
     assert product_data.description == "A mobile phone with advanced features"
-    assert product_data.price == 999.99
+    assert product_data.price == 999.999
     assert product_data.quantity == 100
 
