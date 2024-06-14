3be5f4962e3935437d8ebf700e9cb1c2682e1018
e8olution <evzen.fc@gmail.com>
2024-06-14 16:40:10 +0200

    1.done 2.done 3.done 3*.done 4.done 4*.done

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 #создание_класса
 class Category:
     # атрибуты
-    # типы_данных
     name: str
     description: str
     products: list
@@ -10,44 +9,69 @@ class Category:
     count_uniq_cat = 0
 
     # конструктор_класса
-    # __init__
     def __init__(self,name,description,products):
         # self
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.count_uniq_cat += len(set([product.name for product in products]))
         Category.count_categories += 1
+    #Добавление товара в список
+    def add_product(self, product):
+        return self.__products.append(product)
 
+    # Выыод товаров в формате: Продукт, 80 руб. Остаток: 15 шт.
+    #декоратор(getter,setter,deleter)
+    @property
+    def products(self):
+        #Геттер
+        return [f'{Product.name},{Product.price}руб. Остаток: {Product.quantity} шт.' for Product in self.__products]
+
+def __repr__(self):
+    return f'Product(name={self.name}, description={self.description}, price={self.price}, quantity={self.quantity})'
 
 #создание_класса
- # атрибуты
- #типы_данных
 class Product:
     # атрибуты
-    # типы_данных
     name: str
     description: str
     price: float
     quantity: int
 
-# конструктор_класса
-# __init__
+    # конструктор_класса
     def __init__(self,name,description,price,quantity):
         # self
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
 
-#    def __eq__(self, other):
-#        if isinstance(other, Product):
-#            return (self.name == other.name and
-#                    self.description == other.description and
-#                    self.price == other.price and
-#                    self.quantity == other.quantity)
-#        return False
-
-
-def __repr__(self):
-    return f'Product(name={self.name}, description={self.description}, price={self.price}, quantity={self.quantity})'
+    #Декоратор(статический)
+    @staticmethod
+    def new_product(name, description, price, quantity, product_list):
+        # Проверка наличия дубликатов
+        for product in product_list:
+            if product.name == name:
+                product.quantity += quantity
+                product.price = max(product.price, price)
+                return product
+        # Создание нового продукта, если дубликат не найден
+        new_product = Product(name, description, price, quantity)
+        product_list.append(new_product)
+        return new_product
+    #Геттер для цены
+    @property
+    def price(self):
+        return self._price
+    #Сеттер для цены
+    @price.setter
+    def price(self,value):
+        if value <= 0:
+            print("Цена введена некорректная")
+            return
+        # Подтверждение пользователем понижения цены товара
+        if value < self._price:
+            confirm = input (f"Подтвердите снижение цены с {self._price} до {value} (y/n)")
+            if confirm.lower() != 'y':
+                return
+        self._price = value
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from src.main import Product
 from src.main import Category
+from unittest.mock import patch
 
 
 
@@ -25,10 +26,34 @@ def test_init_category(category_data):
 
 @pytest.fixture
 def product_data():
-    return Product('Samsung','A mobile phone with advanced features',999.999, 100)
+    return (Product('Samsung','A mobile phone with advanced features',999.999, 100)
+    )
+
 def test_init_product(product_data):
     assert product_data.name == "Samsung"
     assert product_data.description == "A mobile phone with advanced features"
     assert product_data.price == 999.999
     assert product_data.quantity == 100
 
+product1 = Product("Apple", "Fresh Apple", 10.0, 50)
+
+
+def test_price(product_data):
+    assert product_data._price == 999.999
+def test_price_lower_yes(product_data):
+    with patch('builtins.input', return_value='y'):
+        product_data.price = 99.999
+    assert product_data._price == 99.999
+def test_price_lower_no(product_data):
+    with patch('builtins.input', return_value='n'):
+        product_data.price = 99.999
+    assert product_data._price == 999.999
+
+def test_price_lower_no(product_data):
+    with patch('builtins.input'):
+        product_data.price = -99.999
+    assert product_data._price == 999.999
+def test_price_lower_no(product_data):
+    with patch('builtins.input'):
+        product_data.price = 0
+    assert product_data._price == 999.999
