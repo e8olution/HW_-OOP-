00e13331ec49b59f24ab81ef2082b05c4d0b1be3
e8olution <evzen.fc@gmail.com>
2024-06-18 17:36:22 +0200

    hw 14.1 1.done 2.done

--- src/main.py
+++ src/main.py
@@ -2,67 +2,72 @@
 #создание_класса
 class Category:
     # атрибуты
-    name: str
-    description: str
+    name_category: str
+    description_category: str
     products: list
     count_categories = 0
     count_uniq_cat = 0
 
     # конструктор_класса
-    def __init__(self,name,description,products):
+    def __init__(self,name_category,description_category,products):
         # self
-        self.name = name
-        self.description = description
+        self.name_category = name_category
+        self.description_category = description_category
         self.__products = products
-        Category.count_uniq_cat += len(set([product.name for product in products]))
+        Category.count_uniq_cat += len(set([product.name_product for product in products]))
         Category.count_categories += 1
     #Добавление товара в список
     def add_product(self, product):
         return self.__products.append(product)
 
     # Выыод товаров в формате: Продукт, 80 руб. Остаток: 15 шт.
-    #декоратор(getter,setter,deleter)
+    # декоратор(getter,setter,deleter)
     @property
     def products(self):
         #Геттер
-        return [f'{Product.name},{Product.price}руб. Остаток: {Product.quantity} шт.' for Product in self.__products]
+        return [f'{product.name_product},{product.price}руб. Остаток: {product.quantity} шт.' for product in self.__products]
+    def __str__(self):
+        return f'{self.description_category}, количество продуктов: {self.count_categories} шт.'
+    def __len__(self):
+        return sum(product.quantity for product in self.__products)
 
-def __repr__(self):
-    return f'Product(name={self.name}, description={self.description}, price={self.price}, quantity={self.quantity})'
+
+##########################__Product__##########################
 
 #создание_класса
 class Product:
     # атрибуты
-    name: str
-    description: str
+    name_product: str
+    description_product: str
     price: float
     quantity: int
 
     # конструктор_класса
-    def __init__(self,name,description,price,quantity):
+    def __init__(self,name_product,description_product,price,quantity):
         # self
-        self.name = name
-        self.description = description
+        self.name_product = name_product
+        self.description_product = description_product
         self._price = price
         self.quantity = quantity
 
     #Декоратор(статический)
     @staticmethod
-    def new_product(name, description, price, quantity, product_list):
+    def new_product(name_product, description_product, price, quantity, product_list):
         # Проверка наличия дубликатов
         for product in product_list:
-            if product.name == name:
+            if product.name_product == name_product:
                 product.quantity += quantity
                 product.price = max(product.price, price)
                 return product
         # Создание нового продукта, если дубликат не найден
-        new_product = Product(name, description, price, quantity)
+        new_product = Product(name_product, description_product, price, quantity)
         product_list.append(new_product)
         return new_product
     #Геттер для цены
     @property
     def price(self):
         return self._price
+
     #Сеттер для цены
     @price.setter
     def price(self,value):
@@ -75,3 +80,11 @@ class Product:
             if confirm.lower() != 'y':
                 return
         self._price = value
+
+    def __str__(self):
+        return f'({self.name_product}, {self.price} руб. Остаток: {self.quantity} шт.)'
+
+    def __add__(self, other):
+        total_price = self.price * self.quantity + other.price * other.quantity
+        total_quantity = self.quantity + other.quantity
+        return total_price, total_quantity
--- tests/test_main.py
+++ tests/test_main.py
@@ -16,9 +16,9 @@ def category_data():
                     )
 def test_init_category(category_data):
     # Проверка корректности инициализации объектов класса Category
-    assert category_data.name == 'Electronics'
-    assert category_data.description == 'Smartphones'
-    assert category_data.description == 'Smartphones'
+    assert category_data.name_category == 'Electronics'
+    assert category_data.description_category == 'Smartphones'
+    assert category_data.description_category == 'Smartphones'
     assert len(category_data.products) == 2
 
     assert category_data.count_uniq_cat == 2
@@ -30,8 +30,8 @@ def product_data():
     )
 
 def test_init_product(product_data):
-    assert product_data.name == "Samsung"
-    assert product_data.description == "A mobile phone with advanced features"
+    assert product_data.name_product == "Samsung"
+    assert product_data.description_product == "A mobile phone with advanced features"
     assert product_data.price == 999.999
     assert product_data.quantity == 100
 
